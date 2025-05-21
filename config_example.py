e1ddbaba83998ee44700cbc2c704fb68173d83fd
Rinat0880 <176410022+Rinat0880@users.noreply.github.com>
2025-05-21 15:30:33 +0500

    Update config_example.py

--- config_example.py
+++ config_example.py
@@ -3,7 +3,7 @@ login = "your_login_here"
 password = "your_password_here"
 
 # вот тут массив предметов
-subject_name = [
+subject_names = [
     "subject1",        
     "subject2",       
     "subject3",  
